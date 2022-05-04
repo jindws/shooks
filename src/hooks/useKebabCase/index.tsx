3986fc4c04ed5ff7842065e0347cf7fe74c4a261
jindw <jindaweihd@163.com>
2022-05-04 18:58:08 +0800

    up useKebabCase

--- src/hooks/useKebabCase/index.tsx
+++ src/hooks/useKebabCase/index.tsx
@@ -2,9 +2,10 @@ import { useMemo } from "react";
 
 function useKebabCase(str: string, line: string = "-"): string {
   return useMemo(() => {
+    const regExp = new RegExp(`([^${line}])([A-Z])`, "g");
     return str
-      .replace(new RegExp(`([^${line}])([A-Z])`, "g"), `$1${line}$2`)
-      .replace(new RegExp(`([^${line}])([A-Z])`, "g"), `$1${line}$2`)
+      .replace(regExp, `$1${line}$2`)
+      .replace(regExp, `$1${line}$2`)
       .toLowerCase();
   }, [str, line]);
 }
